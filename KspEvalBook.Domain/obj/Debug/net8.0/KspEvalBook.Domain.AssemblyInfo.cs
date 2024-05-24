c8575d824189b320ef3c408f54fcd3b2383c0924
1002alonso <1002alonso@gmail.com>
2024-05-24 10:41:47 -0600

    ControlBook|feature| Add librearias  EntityCore|

--- KspEvalBook.Domain/obj/Debug/net8.0/KspEvalBook.Domain.AssemblyInfo.cs
+++ KspEvalBook.Domain/obj/Debug/net8.0/KspEvalBook.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KspEvalBook.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58a8e9b1b2c4e9d78cca5790da037d3c1fe474c")]
 [assembly: System.Reflection.AssemblyProductAttribute("KspEvalBook.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KspEvalBook.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KspEvalBook.Infrastructure/obj/Debug/net8.0/KspEvalBook.Infrastructure.AssemblyInfo.cs
+++ KspEvalBook.Infrastructure/obj/Debug/net8.0/KspEvalBook.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KspEvalBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58a8e9b1b2c4e9d78cca5790da037d3c1fe474c")]
 [assembly: System.Reflection.AssemblyProductAttribute("KspEvalBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KspEvalBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
